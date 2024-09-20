df3915c78f25bd0c3d559d121cc940167f23160d
Octavian Sulic <octavian.sulic@hogarth.com>
2024-09-20 12:05:13 +0300

    Add swagger to dependencies.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set up global validation pipes
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }));
+
+  // Set up swagger
+  const config = new DocumentBuilder()
+    .setTitle('Tutorial')
+    .setDescription('Tutorial API description here')
+    .setVersion('1.0')
+    .addTag('tutorial')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3003);
 }
 bootstrap();
